494a9d5ff20cb3ffd5932e3d14210d1c5c94e182
Bohdan <bodya19981997@gmail.com>
2020-05-08 18:36:32 +0300

    Update Name

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const AppHeader = () => {
     return(
         <div className="app-header d-flex">
-            <h1>Ivan Petrychenko</h1>
+            <h1>Bohdan Strukov</h1>
             <h2>5 записей, из них понравилось 0</h2>
         </div>
     )
